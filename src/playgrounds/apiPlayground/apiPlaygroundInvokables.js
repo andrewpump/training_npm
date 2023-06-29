c1e528f20d9433537fe52f27a19569c613e195fd
Azim Ahmed <azim.ahmed7@gmail.com>
2023-06-29 17:30:20 -0400

    api playground

---
+++ src/playgrounds/apiPlayground/apiPlaygroundInvokables.js
@@ -0,0 +1,29 @@
+// @ts-check
+import { OpenApiInvokable } from "@buildwithlayer/sdk";
+
+import store from "../../app/store";
+import { setResponse } from "./apiPlaygroundSlice";
+
+import countryDataApiSpec from "./country-data-api.json";
+
+const ApiPlaygroundInvokables = {
+  name: "Api Playground",
+  invokables: [
+    new OpenApiInvokable({
+      name: "countriesApi",
+      description:
+        "useful for when human wants to know anything about countries",
+      spec: countryDataApiSpec,
+      // onResponse: async (response) => {
+      //   store.dispatch(setResponse(response));
+      //   return "Response set!";
+      // },
+      headers: {
+        "Content-Type": "application/json",
+        "X-BLOBR-KEY": process.env.REACT_APP_X_BLOBR_KEY,
+      },
+    }),
+  ],
+};
+
+export { ApiPlaygroundInvokables };
