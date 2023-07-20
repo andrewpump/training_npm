86341d6db2dc702adbc848c6eed901a112d452f5
Lucas Gismondi <lucas@gismondigroup.com>
2023-07-20 11:25:04 -0400

    fix: unused import build issues

--- src/playgrounds/apiPlayground/apiPlaygroundInvokables.js
+++ src/playgrounds/apiPlayground/apiPlaygroundInvokables.js
@@ -1,29 +1,28 @@
 // @ts-check
-import { UnstableOpenApiInvokable } from "@buildwithlayer/sdk";
+import { UnstableOpenApiInvokable } from '@buildwithlayer/sdk';
 
-import store from "../../app/store";
-import { setResponse } from "./apiPlaygroundSlice";
+import store from '../../app/store';
+import { setResponse } from './apiPlaygroundSlice';
 
-import countryDataApiSpec from "./country-data-api.json";
+import countryDataApiSpec from './country-data-api.json';
 
 const ApiPlaygroundInvokables = {
-  name: "Api Playground",
-  invokables: [
-    new UnstableOpenApiInvokable({
-      name: "countriesApi",
-      description:
-        "useful for when human wants to know anything about countries",
-      spec: countryDataApiSpec,
-      // onResponse: async (response) => {
-      //   store.dispatch(setResponse(response));
-      //   return "Response set!";
-      // },
-      headers: {
-        "Content-Type": "application/json",
-        "X-BLOBR-KEY": `${process.env.REACT_APP_X_BLOBR_KEY}`,
-      },
-    }),
-  ],
+    name: 'Api Playground',
+    invokables: [
+        new UnstableOpenApiInvokable({
+            name: 'countriesApi',
+            description: 'useful for when human wants to know anything about countries',
+            spec: countryDataApiSpec,
+            onResponse: async (response) => {
+                store.dispatch(setResponse(response));
+                return 'Response set!';
+            },
+            headers: {
+                'Content-Type': 'application/json',
+                'X-BLOBR-KEY': `${process.env.REACT_APP_X_BLOBR_KEY}`,
+            },
+        }),
+    ],
 };
 
 export { ApiPlaygroundInvokables };
